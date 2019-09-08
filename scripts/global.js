381c6c3884b832b385c2690ce4fe3a0fde04b32d
s3744010 <s3744010@student.rmit.edu.au>
2019-09-09 03:19:30 +1000

    Reponsive

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,45 @@
 function headerResize() {
-	var content = document.querySelector("header");
-	var displayWidth = window.innerWidth * 100/1920;
+	var content = document.querySelector(".intro");
+	var displayWidth = window.innerWidth * 145/1920;
 	content.style.height = displayWidth.toString() + "vh";
 }
+
+function showText(n, size) {
+	var status = "less";
+	var content = document.getElementsByClassName("paragraph").item(n);
+	var readMore = content.innerHTML;
+	var lessText = readMore.substr(0, size);
+	function applyTextButton() {
+		if (window.innerWidth < 767){
+			if (status == "less") {
+				content.innerHTML = lessText + "<a href='' class='read-more-link'><br>Show More</a>";
+				var showMoreButton = document.querySelector("a.read-more-link");
+				showMoreButton.addEventListener('click', showMore);
+			} else {
+				content.innerHTML = readMore + "<a href='' class='show-less-link'>Show Less</a>";
+				var showLessButton = document.querySelector("a.show-less-link");
+				showLessButton.addEventListener('click', showLess);
+			}
+		} else {
+			content.innerHTML = readMore;
+		}
+            
+		function showMore(e) {
+			e.preventDefault();
+			content.innerHTML = readMore + "<a href='' class='show-less-link'>Show Less</a>";
+			var showLessButton = document.querySelector("a.show-less-link");
+			showLessButton.addEventListener('click', showLess);
+			status = "more";
+		}
+        
+		function showLess(e) {
+			e.preventDefault();	
+			content.innerHTML = lessText + "<a href='' class='read-more-link'><br>Show More</a>";
+			document.querySelector("a.read-more-link").addEventListener('click', showMore);
+		status = "less";
+		}
+	}
+	applyTextButton();
+	window.addEventListener("resize", applyTextButton);
+}
+
